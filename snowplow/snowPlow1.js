69cdf8ad2d5be5ab14d6a4bdc730d54ae5ded04b
IssacLo <kitttlooo@gmail.com>
2022-01-21 17:58:16 +0800

    add click function

--- snowplow/snowPlow1.js
+++ snowplow/snowPlow1.js
@@ -1,13 +1,29 @@
-import { newTracker, trackPageView } from "@snowplow/browser-tracker";
+import { newTracker, trackPageView, enableActivityTracking } from "@snowplow/browser-tracker";
+import {
+  LinkClickTrackingPlugin,
+  enableLinkClickTracking,
+} from "@snowplow/browser-plugin-link-click-tracking";
 import { trackSocialInteraction, SiteTrackingPlugin } from "@snowplow/browser-plugin-site-tracking";
 
 export function initializeSnowplow() {
   newTracker("sp", "https://4e30d199-be88-4f6b-8578-9c01f4f7a020.app.try-snowplow.com", {
     appId: "try-snowplow",
     platform: "web",
-    cookieDomain: "https://my-website.com",
-    plugins: [SiteTrackingPlugin()],
+    cookieDomain: "localhost:3000",
+    // cookieDomain: null,
+    // discoverRootDomain: true,
+    // cookieName: "_sp_",
+    // cookieSameSite: "Lax", // Recommended
+    // cookieSecure: true,
+    plugins: [SiteTrackingPlugin(), LinkClickTrackingPlugin()],
   });
+
+  enableActivityTracking({
+    minimumVisitLength: 30,
+    heartbeatDelay: 10,
+  });
+
+  enableLinkClickTracking();
 
   trackPageView();
   console.log("456");
